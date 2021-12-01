1c31d3c0377d4f1a984b6e450ad4aec011c1c983
Momin Khan <foolmoron@gmail.com>
2021-12-01 13:35:21 -0500

    fix runner

--- bin/aoc.dart
+++ bin/aoc.dart
@@ -1,107 +1,107 @@
 import '../aoc0.dart';
-// import 'package:aoc/aoc1.dart';
-// import 'package:aoc/aoc2.dart';
-// import 'package:aoc/aoc3.dart';
-// import 'package:aoc/aoc4.dart';
-// import 'package:aoc/aoc5.dart';
-// import 'package:aoc/aoc6.dart';
-// import 'package:aoc/aoc7.dart';
-// import 'package:aoc/aoc8.dart';
-// import 'package:aoc/aoc9.dart';
-// import 'package:aoc/aoc10.dart';
-// import 'package:aoc/aoc11.dart';
-// import 'package:aoc/aoc12.dart';
-// import 'package:aoc/aoc13.dart';
-// import 'package:aoc/aoc14.dart';
-// import 'package:aoc/aoc15.dart';
-// import 'package:aoc/aoc16.dart';
-// import 'package:aoc/aoc17.dart';
-// import 'package:aoc/aoc18.dart';
-// import 'package:aoc/aoc19.dart';
-// import 'package:aoc/aoc20.dart';
-// import 'package:aoc/aoc21.dart';
-// import 'package:aoc/aoc22.dart';
-// import 'package:aoc/aoc23.dart';
-// import 'package:aoc/aoc24.dart';
-// import 'package:aoc/aoc25.dart';
+import '../aoc1.dart';
+// import '../aoc2.dart';
+// import '../aoc3.dart';
+// import '../aoc4.dart';
+// import '../aoc5.dart';
+// import '../aoc6.dart';
+// import '../aoc7.dart';
+// import '../aoc8.dart';
+// import '../aoc9.dart';
+// import '../aoc10.dart';
+// import '../aoc11.dart';
+// import '../aoc12.dart';
+// import '../aoc13.dart';
+// import '../aoc14.dart';
+// import '../aoc15.dart';
+// import '../aoc16.dart';
+// import '../aoc17.dart';
+// import '../aoc18.dart';
+// import '../aoc19.dart';
+// import '../aoc20.dart';
+// import '../aoc21.dart';
+// import '../aoc22.dart';
+// import '../aoc23.dart';
+// import '../aoc24.dart';
+// import '../aoc25.dart';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.contains('0')) {
     print('Hello world: ${calculate()}!');
   }
   if (arguments.isEmpty || arguments.contains('1')) {
-    // print('AoC$arg:\n${aoc1()}');
+    print('AoC1:\n${aoc1()}');
   }
   if (arguments.isEmpty || arguments.contains('2')) {
-    // print('AoC$arg:\n${aoc2()}');
+    // print('AoC2:\n${aoc2()}');
   }
   if (arguments.isEmpty || arguments.contains('3')) {
-    // print('AoC$arg:\n${aoc3()}');
+    // print('AoC3:\n${aoc3()}');
   }
   if (arguments.isEmpty || arguments.contains('4')) {
-    // print('AoC$arg:\n${aoc4()}');
+    // print('AoC4:\n${aoc4()}');
   }
   if (arguments.isEmpty || arguments.contains('5')) {
-    // print('AoC$arg:\n${aoc5()}');
+    // print('AoC5:\n${aoc5()}');
   }
   if (arguments.isEmpty || arguments.contains('6')) {
-    // print('AoC$arg:\n${aoc6()}');
+    // print('AoC6:\n${aoc6()}');
   }
   if (arguments.isEmpty || arguments.contains('7')) {
-    // print('AoC$arg:\n${aoc7()}');
+    // print('AoC7:\n${aoc7()}');
   }
   if (arguments.isEmpty || arguments.contains('8')) {
-    // print('AoC$arg:\n${aoc8()}');
+    // print('AoC8:\n${aoc8()}');
   }
   if (arguments.isEmpty || arguments.contains('9')) {
-    // print('AoC$arg:\n${aoc9()}');
+    // print('AoC9:\n${aoc9()}');
   }
   if (arguments.isEmpty || arguments.contains('10')) {
-    // print('AoC$arg:\n${aoc10()}');
+    // print('AoC10:\n${aoc10()}');
   }
   if (arguments.isEmpty || arguments.contains('11')) {
-    // print('AoC$arg:\n${aoc11()}');
+    // print('AoC11:\n${aoc11()}');
   }
   if (arguments.isEmpty || arguments.contains('12')) {
-    // print('AoC$arg:\n${aoc12()}');
+    // print('AoC12:\n${aoc12()}');
   }
   if (arguments.isEmpty || arguments.contains('13')) {
-    // print('AoC$arg:\n${aoc13()}');
+    // print('AoC13:\n${aoc13()}');
   }
   if (arguments.isEmpty || arguments.contains('14')) {
-    // print('AoC$arg:\n${aoc14()}');
+    // print('AoC14:\n${aoc14()}');
   }
   if (arguments.isEmpty || arguments.contains('15')) {
-    // print('AoC$arg:\n${aoc15()}');
+    // print('AoC15:\n${aoc15()}');
   }
   if (arguments.isEmpty || arguments.contains('16')) {
-    // print('AoC$arg:\n${aoc16()}');
+    // print('AoC16:\n${aoc16()}');
   }
   if (arguments.isEmpty || arguments.contains('17')) {
-    // print('AoC$arg:\n${aoc17()}');
+    // print('AoC17:\n${aoc17()}');
   }
   if (arguments.isEmpty || arguments.contains('18')) {
-    // print('AoC$arg:\n${aoc18()}');
+    // print('AoC18:\n${aoc18()}');
   }
   if (arguments.isEmpty || arguments.contains('19')) {
-    // print('AoC$arg:\n${aoc19()}');
+    // print('AoC19:\n${aoc19()}');
   }
   if (arguments.isEmpty || arguments.contains('20')) {
-    // print('AoC$arg:\n${aoc20()}');
+    // print('AoC20:\n${aoc20()}');
   }
   if (arguments.isEmpty || arguments.contains('21')) {
-    // print('AoC$arg:\n${aoc21()}');
+    // print('AoC21:\n${aoc21()}');
   }
   if (arguments.isEmpty || arguments.contains('22')) {
-    // print('AoC$arg:\n${aoc22()}');
+    // print('AoC22:\n${aoc22()}');
   }
   if (arguments.isEmpty || arguments.contains('23')) {
-    // print('AoC$arg:\n${aoc23()}');
+    // print('AoC23:\n${aoc23()}');
   }
   if (arguments.isEmpty || arguments.contains('24')) {
-    // print('AoC$arg:\n${aoc24()}');
+    // print('AoC24:\n${aoc24()}');
   }
   if (arguments.isEmpty || arguments.contains('25')) {
-    // print('AoC$arg:\n${aoc25()}');
+    // print('AoC25:\n${aoc25()}');
   }
 }
